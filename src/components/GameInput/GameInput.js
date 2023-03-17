8dff27666ae895a26c871f5c58276230fd186910
Rao BJM <rao@bjmdigital.com.au>
2023-03-17 21:04:24 +0500

    Added GameInput component - exercise-1

---
+++ src/components/GameInput/GameInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function GameInput() {
+  const [guessInput, setGuessInput] = React.useState("");
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (guessInput.length !== 5) {
+          window.alert("Word should be 5 characters long");
+          return;
+        }
+        console.log(guessInput);
+        setGuessInput("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guessInput}
+        onChange={(event) => {
+          const newGuess = event.target.value;
+
+          setGuessInput(newGuess.toUpperCase());
+        }}
+      />
+    </form>
+  );
+}
+
+export default GameInput;
